2b1d6ab3e7df49689ecd6a84c0215ccab03714e6
Sumit Majumdar <sumit25ish@gmail.com>
2022-04-17 17:22:00 +0530

    stuff

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React, { useEffect, useRef } from 'react';
-import { consumeDonut, initializeApp } from 'web-sdk';
+import React, { useEffect, useRef, useState } from 'react';
+import { consumeLamp, initializeClient } from 'web-sdk';
+
+const platformId = '625ac3ef3f6501b9179644cc';
+const signature =
+  'eT5G8XorUe/bn2E2Q1bBTuOiTuQ39cSeQ+YFHTN+gMoMpTmjKSpgKZ1riAJVibUaxPmTWTb8Twq5RWqRK7GxMGCYtH5tnED6RiFW1gf++iHx+aBCInG+0EMPsRai2gdoLgBYbObvYl+ma8WrmlwUKrjvmCoYu/uFkz2OU7veeT+GMJEkISLbLzYml2PDcp+yYzrjGhx9tlZpkYd/E1CYTOaJ+zMyVj1FJeocWaJWnlqte+zhh06MRjVSU7Xdd1hWYI2jVnhmN4SZLDtNpnnzSGeuJW9g2txduLamyfzd30FEVBuBOfpUSVCiYD1AEAkuiTxlNQncSsGKnxqvLO5t7g==';
 
 function App() {
   const mountTarget = useRef();
+  const lampInitialized = useRef(false);
+  const [donutInit, setDonutInit] = useState(false);
 
   useEffect(() => {
-    if (mountTarget && mountTarget.current) {
-      mountTarget.current.innerHTML = '';
-    }
-    initializeApp({
-      accessKey: 'sample_access_key',
-      clientId: '622c74a2a3d36035c13c564f',
-      additional: {
-        refId: 'fake_ref',
-        role: 'learner'
-      },
-      mountTarget: mountTarget.current
-    });
-    consumeDonut({
-      userId: 'fake_ref',
-      role: 'tutor'
-    });
+    initializeClient({
+      platformId,
+      signature,
+      role: 'learner',
+      refId: 'learner1_ref_id'
+    })
+      .then(() => {
+        lampInitialized.current = true;
+        console.log('lampInitialized', lampInitialized.current);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }, []);
+
+  const startDonut = () => {
+    if (lampInitialized.current) {
+      if (mountTarget && mountTarget.current) {
+        mountTarget.current.innerHTML = '';
+      }
+      consumeLamp({
+        type: 'donut',
+        metaData: {
+          recorded: true
+        },
+        target: mountTarget.current
+      });
+      setDonutInit(true);
+    } else {
+      window.alert('Lamp not initialized');
+    }
+  };
 
   // const onSubmit = useCallback(
   //   (e) => {
@@ -36,7 +57,14 @@ function App() {
   // );
 
   return (
-    <div className="h-screen">
+    <div className="h-screen flex">
+      <div className={`w-56 bg-yellow-400 py-12 px-4 ${donutInit ? 'hidden' : ''}`}>
+        <button
+          className="px-6 py-3 cursor-pointer bg-gray-300 hover:bg-gray-50 text-lg"
+          onClick={startDonut}>
+          Start Donut
+        </button>
+      </div>
       <div className="h-full w-full" ref={mountTarget} />
       {/* <form onSubmit={onSubmit}>
         <div className="flex flex-col p-12 shadow-md rounded-md w-96">
